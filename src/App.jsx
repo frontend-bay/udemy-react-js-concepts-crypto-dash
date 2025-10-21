ea1de80fca964d6a36c5d1076c659b242c8aa2c1
amit96pal <amit96pal.ap@gmail.com>
2025-10-21 15:51:19 +0530

    till filter

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import {useEffect, useState} from "react";
+import CoinCards from "./components/CoinCards.jsx";
+import LimitSelector from "./components/LimitSelector.jsx";
+import FilterInput from "./components/FilterInput.jsx";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const App = () => {
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [limit, setLimit] = useState(10);
+    const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        const fetchCoins = async () => {
+            try {
+                const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`);
+                if (!res.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+                const data = await res.json();
+                console.log(data);
+                setCoins(data);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchCoins();
+    }, [limit]);
+
+    // useEffect(() => {
+    //     fetch(API_URL)
+    //         .then((res) => {
+    //             if(!res.ok) throw new Error("Failed to fetch data");
+    //             return res.json();
+    //         })
+    //         .then((data) => {
+    //             console.log(data);
+    //             setCoins(data);
+    //             setLoading(false);
+    //         })
+    //         .catch((err) => {
+    //             setError(err.message);
+    //             setLoading(false);
+    //         })
+    // }, [])
+
+
+    const filterCoins = coins.filter((coin) => {
+        return coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+            coin.symbol.toLowerCase().includes(filter.toLowerCase());
+    });
+
+    return (
+        <div>
+            <h1>Crypto Dash</h1>
+            {loading && <p>loading...</p>}
+            {error && <div className='error'>{error}</div>}
+
+            <div className="top-controls">
+                <FilterInput filter={filter} onFilterChange={setFilter}></FilterInput>
+                <LimitSelector limit={limit} onLimitChange={setLimit}></LimitSelector>
+
+            </div>
+
+            {!loading && !error && (
+                <main className="grid">
+                    {filterCoins.length > 0 ? (filterCoins.map((coin) => (
+                        <CoinCards key={coin.id} coin={coin}></CoinCards>
+                    ))) : <p>No matching coins</p>}
+                </main>
+            )}
+        </div>
+    )
+}
+
+export default App;
